22fd54824ee6c398ebdebd25a957b797b2cb664f
Lorena Pantano <lorena.pantano@gmail.com>
2016-10-04 17:11:13 -0400

    Improve parsing snpeff info

--- scripts/split_conversion.sh
+++ scripts/split_conversion.sh
@@ -1,15 +1,19 @@
 cd ~/scratch/church_mammoth/mammoth_vc/work/joint/gatk-haplotype-joint/batch1
 mkdir split && cd split
+mkdir log
+
+gunzip ../batch1-joint-effects-filterSNP-filterINDEL-gatkclean.vcf.gz
 
 split ../batch1-joint-effects-filterSNP-filterINDEL-gatkclean.vcf
 
+grep "^#" ../batch1-joint-effects-filterSNP-filterINDEL-gatkclean.vcf > header
 
-for F in `ls * | grep -v tsv | grep -v "h$"` ; do
+for F in `ls x* | grep -v tsv | grep -v ".h"` ; do
     echo $F
     `cat header $F > $F.h`
     if [ ! -e $F"-parsed-flank.tsv"  ] ; then
         # echo do
-        bsub -W 10:00 -R "rusage[mem=8000]" -n  2 -q mcore  ~/scratch/church_mammoth/conda/bin/python ~/scratch/church_mammoth/mammoth_code/scripts/parse_vcf.py $F.h
+        bsub -eo log/$F.o -W 10:00 -R "rusage[mem=8000]" -n  2 -q parallel  ~/scratch/church_mammoth/conda/bin/python ~/scratch/church_mammoth/mammoth_code/scripts/parse_vcf.py $F.h
     fi
 done
 
@@ -20,4 +24,3 @@ cat <(grep -w chrom xab-parsed-flank.tsv)  <( cat merged-parsed-flank.tsv) > mer
 
 awk '{start=$2-150; if (start<0){start=0}; print $1"\t"$2-150"\t"$2+150}' ../batch1-joint-effects-filterSNP-filterINDEL-gatkclean.vcf |grep -v "##" > merged.bed
 
-betdo
