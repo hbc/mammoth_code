a2565708fe3a5747a5a5f48e373bfd313b6be128
Lorena Pantano <lorena.pantano@gmail.com>
2016-10-20 09:42:22 -0400

    Add new scripts for parsing

---
+++ scripts/merge_func_table.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import pybedtools
+import shutil
+from collections import defaultdict
+
+hg19="/home/lp113/scratch/church_mammoth/mammoth_code/test/ann/hg19_pos.bed"
+variants="../all_genomes.xls"
+ann="/home/lp113/scratch/church_mammoth/mammoth_code/test/ann/hg19_ann.txt"
+
+hg19_map = {}
+with open(hg19) as inh:
+    for line in inh:
+        cols = line.strip().split()
+        hg19_map[cols[3]] = "%s:%s" % (cols[0], int(cols[1]) + 1)
+
+ann_map = defaultdict(dict)
+with open(ann) as inh:
+    for line in inh:
+        cols = line.strip().split()
+        idx = "chr%s:%s" % (cols[7], cols[8])
+        cols[4] = cols[4] if cols[4] != "X" else "STOP"
+        cols[5] = cols[5] if cols[5] != "X" else "STOP"
+        ann_map[idx].update({"%s %s" % (cols[4],cols[5]): cols[34]})
+        # print ann_map[idx]
+
+def _get_change(cols):
+    if cols[7] != "NA":
+        ref = cols[7]
+    elif cols[27]:
+        ref = aa_name[cols[27][2:5]]
+    if cols[11] != "NA":
+        alt = cols[11]
+    elif cols[18] != "NA":
+        alt = cols[18]
+    else:
+        if cols[27].endswith("*"):
+            alt = "STOP"
+        elif cols[27][-3:] not in aa_name:
+            alt = "NA"
+        else:
+            alt = aa_name[cols[27][-3:]]
+    return [ref, alt]
+
+aa_name = {'Ala': 'A', 'Arg': 'R', 'Asn': 'N', 'Asp': 'D', 'Cys': 'C',
+           'Gln': 'Q', 'Glu': 'E', 'Gly': 'G', 'His': 'H', 'Ile': 'I',
+           'Leu': 'L', 'Lys': 'K', 'Met': 'M', 'Phe': 'F', 'Pro': 'P',
+           'Ser': 'S', 'Thr': 'T', 'Trp': 'W', 'Tyr': 'Y', 'Val': 'V',
+           'Ter': 'STOP', '': 'NA'}
+
+with open(variants) as inh:
+    for line in inh:
+        if line.find("gene") > -1:
+            print "\t".join(line.strip().split("\t") + ["polyphen2"])
+            continue
+        cols=[c.replace("\"", "") for c in line.strip().split("\t")]
+        chrom, pos=cols[8], int(cols[9])
+        idx = "%s:%s" % (chrom.replace("\"", ""), pos)
+        score = "NA"
+        if idx in hg19_map:
+            if hg19_map[idx] in ann_map:
+                change = _get_change(cols)
+                score = []
+                for item in ann_map[hg19_map[idx]]:
+                    score.append(ann_map[hg19_map[idx]][item])
+                    if " ".join(change) == item:
+                        score = score[-1]
+                        break
+                score = ",".join(score)
+        print "\t".join(cols + [score])
--- scripts/vcf_parsing.sh
+++ scripts/vcf_parsing.sh
@@ -26,3 +26,15 @@ awk '{start=$2-150; if (start<0){start=0}; print $1"\t"$2-150"\t"$2+150}' ../bat
 
 bsub -eo log/fasta.o -W 40:00 -R "rusage[mem=8000]" -n  2 -q parallel  ~/scratch/church_mammoth/conda/bin/python ~/scratch/church_mammoth/mammoth_code/scripts/get_african_sequence.py
 
+# conver to human to use snpsniff
+grep -v "^#"  ../batch1-joint-effects-filterSNP-filterINDEL-gatkclean.vcf | awk '{print $1"\t"$2"\t"$2+1"\t"$1":"$2}' > vcf_pos.bed
+
+# CrossMap.py bed ~/scratch/church_mammoth/tools/liftover/loxAfr3ToHg38.over.chain.gz vcf_pos.bed vcf_pos_hg38.bed
+
+# ~/scratch/church_mammoth/conda/bin/python ~/scratch/church_mammoth/mammoth_code/scripts/merge_bed_vcf.py vcf_pos_hg38.vcf
+# bedtools sort -i vcf_pos_hg38.vcf.gz -faidx /groups/bcbio/bcbio/genomes/Hsapiens/hg38/seq/hg38.fa.fai -header > vcf_pos_sort_hg38.vcf
+# bgzip vcf_pos_hg38_sort.vcf
+# bcftools index vcf_pos_hg38_sort.vcf.gz
+# java -jar ~/scratch/church_mammoth/tools/snpEff/SnpSift.jar dbnsfp -db ~/scratch/church_mammoth/tools/snpEff/db/hg19/dbNSFP/dbNSFP3.2.txt.gz  vcf_pos_hg38_sort.vcf.gz
+
+
