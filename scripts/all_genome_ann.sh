92de7406fbb10d28e96a9194aaecaf13d55d6120
Lorena Pantano <lorena.pantano@gmail.com>
2016-12-05 10:55:30 -0500

    change output for annotation

--- scripts/all_genome_ann.sh
+++ scripts/all_genome_ann.sh
@@ -1,4 +1,4 @@
-cd ~/scratch/church_mammoth/mammoth_code/test/ann
+cd ~/scratch/church_mammoth/mammoth_code/table/ann
 cut -f 9-10 ../all_genomes.xls | sed 's/"//g' | sed 1d | awk '{print $1"\t"$2"\t"$2+1"\t"$1":""}' | sort -u > african_pos.bed
 CrossMap.py bed ~/scratch/church_mammoth/tools/liftover/loxAfr3ToHg19.over.chain.gz african_pos.bed  hg19_pos.bed
 awk '{print $1"\t"$2+1}' hg19_pos.bed | sort -k1,1 -k2n > hg19_dbnsfp.bed
